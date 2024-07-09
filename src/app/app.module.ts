76b37f67f09204a9a8d2dfcd2656412aa4a16fe2
irshaghazal <aimmirza2@gmail.com>
2024-07-09 19:58:02 +0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,19 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { CrudComponent } from './crud/crud.component';
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    // CrudComponent
+    
+  ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
-  ],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent
+    CrudComponent
+    
   ],
   bootstrap: [
     AppComponent
