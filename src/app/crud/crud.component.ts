c8ef68797ea90af8fe99a7bcda8c612550f69495
irshaghazal <aimmirza2@gmail.com>
2024-07-11 18:09:15 +0500

    add

--- src/app/crud/crud.component.ts
+++ src/app/crud/crud.component.ts
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
   
 })
 export class CrudComponent {
-  tblHdr = ['Policy Number', 'Policy Holder Name', 'Policy Holder Age', 'Policy Holder Gender', 'Action']
+  tblHdr = ['Policy Number', 'Policy Holder Name', 'Policy Holder Age', 'Policy Holder Gender', 'Action'];
+  isModalOpen = false;
 
+  policyId: string = '';
+  policyHolderName: string = '';
+  age: number = 0;
+  gender: string = '';
+  
+
+  opnMdl() {
+    this.isModalOpen = true;
+  }
+
+  clsMdl() {
+    this.isModalOpen = false;
+  }
+
+  onSubmit() {
+    console.log('Policy ID:', this.policyId);
+    console.log('Policy Holder Name:', this.policyHolderName);
+    console.log('Age:', this.age);
+    console.log('Gender:', this.gender);
+    this.clsMdl();
+  }
 }
