ee2fb3e8a4673c85db4bfa6fe452b8b5201f6d51
irshaghazal <aimmirza2@gmail.com>
2024-07-20 23:09:11 +0500

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,9 +33,3 @@ import { FilterPipe } from './pipes/filter.pipe';
 })
 export class AppModule { }
 
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
